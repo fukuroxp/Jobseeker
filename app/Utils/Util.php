8491a1934ee7f217efbfcb57a0e77faa92f01f5c
Alfarady Raja <radenalfa9@gmail.com>
2020-05-13 21:48:11 +0700

    fix onesignal

--- app/Utils/Util.php
+++ app/Utils/Util.php
@@ -202,10 +202,7 @@ class Util
                     $msg = 'Pembayaran pada penjualan '.$detail['ref_no'].' sejumlah Rp '.$detail['total'].' berhasil dibayarkan';
                 }
             }
-            $this->sendFcmNotification($tokens, [
-                'title' => $title,
-                'contents' => $msg
-            ]);
+            $this->sendWebNotification($tokens, $title, $msg);
         }
 
         Notification::send($target, new CommonNotification($action, $type, $detail));
@@ -237,5 +234,39 @@ class Util
         $downstreamResponse->numberModification();
         
         return $downstreamResponse;
+    }
+
+    private function sendWebNotification($to = [], $title = "", $content = "") {
+        $content = array(
+            "en" => $content,
+            "id" => $content
+        );
+
+        $fields = array(
+            'app_id' => "1cbf6a67-fce0-43ab-87ee-55ed44c6007e",
+            "include_player_ids" => $to,
+            'headings' => array("en" => $title, "id" => $title),
+            'isAnyWeb' => true,
+            'small_icon' =>"ic_stat_onesignal_default",
+            'android_accent_color' => '2196F3',
+            'contents' => $content
+        );
+
+        $fields = json_encode($fields);
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, "https://onesignal.com/api/v1/notifications");
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json',
+            'Authorization: Basic MTA1NTA2YTktNzQ1Ny00ZmMxLWExMzItZmMyMjVlODA5Nzhl'));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+        curl_setopt($ch, CURLOPT_HEADER, FALSE);
+        curl_setopt($ch, CURLOPT_POST, TRUE);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $fields);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+
+        $response = curl_exec($ch);
+        curl_close($ch);
+
+        return $response;
     }
 }
